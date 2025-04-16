8cad0cee52cc92b9495e6c7d979d9fb132c1647c
thiagolopes2024 <157538699+thiagolopes2024@users.noreply.github.com>
2025-04-16 19:31:58 -0300

    Refactor project structure and update database configurations
    - Added new binary file `pg_internal.init` and modified existing binaries.
    - Restored `docker-compose` project in the solution file.
    - Updated `docker-compose.yml` to change database service name and environment variables.
    - Enhanced `Program.cs` with new application services dependency injection.
    - Revised connection strings in `appsettings.Development.json` and `appsettings.json`.
    - Reorganized namespaces in `ServiceCollectionExtensions.cs` and `OrderService.cs`.
    - Modified `OrderItemConfiguration.cs` to adjust property configurations for `OrderItem`.
    - Updated migration files to reflect new migration name and schema changes.
    - Synchronized `ApplicationDbContextModelSnapshot.cs` with updated entity configurations.

---
+++ src/CleanArchitecture.Order.Infrastructure/Migrations/20250416221452_FirstMigration.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace CleanArchitecture.OrderManagement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Client",
+                columns: table => new
+                {
+                    client_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_client", x => x.client_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Order",
+                columns: table => new
+                {
+                    order_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    client_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    tax = table.Column<decimal>(type: "numeric", nullable: false),
+                    status = table.Column<int>(type: "integer", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_order", x => x.order_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderItem",
+                columns: table => new
+                {
+                    item_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    order_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    product_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    amount = table.Column<int>(type: "integer", nullable: false),
+                    value = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_order_item", x => new { x.item_id, x.order_id });
+                    table.ForeignKey(
+                        name: "fk_order_item_order_order_id",
+                        column: x => x.order_id,
+                        principalTable: "Order",
+                        principalColumn: "order_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Client_Name",
+                table: "Client",
+                column: "name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_order_item_order_id",
+                table: "OrderItem",
+                column: "order_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Client");
+
+            migrationBuilder.DropTable(
+                name: "OrderItem");
+
+            migrationBuilder.DropTable(
+                name: "Order");
+        }
+    }
+}
